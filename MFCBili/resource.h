d2c2bafb989dc992166a2f799d430643c5937b9c
l07p <l07p@gmx.de>
2021-05-24 16:30:43 +0200

    also resource.h

--- MFCBili/resource.h
+++ MFCBili/resource.h
@@ -11,6 +11,10 @@
 #define IDC_BUTTON1                     1000
 #define IDC_SUB_EDIT                    1001
 #define IDC_BILI_EDIT                   1002
+#define IDC_BUTTON_GREY                 1003
+#define IDC_EDIT_VAR1                   1004
+#define IDC_SCROLLBAR1                  1005
+#define IDC_TAB1                        1006
 
 // Next default values for new objects
 // 
@@ -18,7 +22,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        131
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1007
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
